1ac0aa05d03fcab45a161a73c60e2742611da5f4
TechnoX0 <meroypaulvincent@gmail.com>
2025-06-13 23:56:02 +0800

    ambot

--- Frontend/src/socket.tsx
+++ Frontend/src/socket.tsx
@@ -1,6 +1,7 @@
 // src/socket.ts
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:5000");
+const RENDER_URL = import.meta.env.BASE_URL || "http://localhost:5000";
+const socket: Socket = io(RENDER_URL);
 
 export default socket;

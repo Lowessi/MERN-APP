8389c79d0e6466e725c42bf62462d47fd4dd4fe0
TechnoX0 <meroypaulvincent@gmail.com>
2025-06-16 02:48:26 +0800

    yes 3

--- Frontend/src/Socket.ts
+++ Frontend/src/Socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-const RENDER_URL = import.meta.env.RENDER_URL || "http://localhost:5000";
+const RENDER_URL = import.meta.env.VITE_RENDER_URL || "http://localhost:5000";
 const socket = io(RENDER_URL);
 export default socket;

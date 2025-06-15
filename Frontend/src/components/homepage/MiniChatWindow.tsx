97085491fdd57494a2b8a93a38029060c5c7a90d
TechnoX0 <meroypaulvincent@gmail.com>
2025-06-16 02:52:15 +0800

    ambot

--- Frontend/src/components/homepage/MiniChatWindow.tsx
+++ Frontend/src/components/homepage/MiniChatWindow.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_RENDER_URL || "http://localhost:5000");
 
 interface MiniChatProps {
     conversation: any;
